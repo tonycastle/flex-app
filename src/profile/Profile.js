fe79e5361457b43b9b25e0ab0dbf0253dd34c416
tonycastle <eo057@OA.PNRAD.NET>
2019-06-21 17:14:45 +0200

    basic file structure created

---
+++ src/profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Component} from 'react'
+
+export default class Profile extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Profile page</h1>
+            </div>
+        )
+    }
+}
