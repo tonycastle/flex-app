e7824bc6341c397af22c0f29b4ffcf0e28e0af49
tonycastle <eo057@OA.PNRAD.NET>
2019-06-24 16:55:41 +0200

    added fetch profile from backend

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Profile from './profile/Profile'
 
+const baseUrl = 'http://localhost:3012/'
+
 ReactDOM.render(
-    <Profile />,
+    <Profile baseURL = {baseUrl} />,
     document.getElementById('root')
 );
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,12 +1,39 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+//import ReactDOM from 'react-dom'
 import { Component} from 'react'
 
 export default class Profile extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            profile: {}
+        }
+    }
+
+    componentDidMount(){
+        fetch(`${this.props.baseURL}profile/1`)
+        .then(function(response){
+            return response.json()
+        })
+        .then(jsonObj => {
+            this.setState({
+                profile: jsonObj
+            })
+        })
+    }
+
+    displayProfile(){
+        const output = Object.entries(this.state.profile).map((e,i) =>{
+            return <p key={i}>{e[0]} : {e[1]}</p>
+        })
+        return output
+    }
+
     render(){
         return(
             <div>
                 <h1>Profile page</h1>
+                {this.displayProfile()}
             </div>
         )
     }
